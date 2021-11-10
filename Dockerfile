d1f63b79015f447f332e2dfafee01e7a07fc1abe
David Gasquez <davidgasquez@gmail.com>
2021-11-10 12:27:14 +0100

    :sparkles: Skeleton

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.10
+FROM python:3.9
 
 COPY requirements.txt /tmp/
 RUN pip install --no-cache-dir -r /tmp/requirements.txt
--- app.py
+++ app.py
@@ -1,3 +1,27 @@
 import streamlit as st
+import pandas as pd
+from causalimpact import CausalImpact
+import datetime
 
 st.title("Causal Impact")
+
+data = st.file_uploader("Upload", type={"csv"})
+
+if data is not None:
+    df = pd.read_csv(data)
+    value_column = st.selectbox("Which column contains the values", df.columns)
+    y = df[value_column]
+
+    date_column = st.selectbox("Which column contains the dates", df.columns)
+    dates = df[date_column]
+
+    intervention = st.date_input(
+        "When did the intervention took place?", datetime.date.today()
+    )
+
+    pre_period = [0, 20]
+    post_period = [21, 29]
+
+    if st.button("RUN"):
+        ci = CausalImpact(y, pre_period, post_period)
+        st.text(ci.summary(output="report"))
